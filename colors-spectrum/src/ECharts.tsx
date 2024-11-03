7916a4a1aece29701010822b19fdd0167811c37c
Travis Bumgarner <travis.bumgarner@gmail.com>
2024-11-03 15:48:25 -0600

    doot doot

--- colors-spectrum/src/ECharts.tsx
+++ colors-spectrum/src/ECharts.tsx
@@ -9,56 +9,61 @@ const ECharts3DScatter = () => {
     if (chartRef.current) {
       const chart = echarts.init(chartRef.current);
 
+      const data = [];
+      for (let r = 0; r < 256; r += 16) {
+        for (let g = 0; g < 256; g += 16) {
+          for (let b = 0; b < 256; b += 16) {
+            data.push([r, g, b, `rgb(${r},${g},${b})`]);
+          }
+        }
+      }
+
       const option = {
-        title: {
-          text: '3D Scatter Plot',
-        },
         tooltip: {},
         xAxis3D: {
           type: 'value',
+          name: 'Red',
+          min: 0,
+          max: 255
         },
         yAxis3D: {
           type: 'value',
+          name: 'Green',
+          min: 0,
+          max: 255
         },
         zAxis3D: {
           type: 'value',
+          name: 'Blue',
+          min: 0,
+          max: 255
         },
         grid3D: {
           viewControl: {
-            projection: 'perspective',
-          },
+            projection: 'perspective'
+          }
         },
         series: [
           {
             type: 'scatter3D',
-            symbolSize: 5,
-            data: [
-              [1, 2, 3],
-              [4, 5, 6],
-              [7, 8, 9],
-              [10, 11, 12],
-              [13, 14, 15],
-            ],
-          },
-        ],
+            symbolSize: 8,
+            data: data,
+            itemStyle: {
+              color: (params: any) => params.value[3]
+            }
+          }
+        ]
       };
 
       chart.setOption(option);
 
-      const handleResize = () => {
-        chart.resize();
-      };
-
-      window.addEventListener('resize', handleResize);
-
       return () => {
-        window.removeEventListener('resize', handleResize);
         chart.dispose();
       };
     }
   }, []);
 
-  return <div ref={chartRef} style={{ width: '100%', height: '500px' }} />;
+  return <div ref={chartRef} style={{ width: '1000px', height: '1000px' }} />;
 };
 
 export default ECharts3DScatter;
