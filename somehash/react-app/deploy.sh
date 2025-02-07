6b6a1cf8c9937a623f997a72e0e9e83299fe6ab6
Travis Bumgarner <travis.bumgarner@gmail.com>
2025-02-07 13:30:16 -0600

    new deploy script for blog

--- somehash/react-app/deploy.sh
+++ somehash/react-app/deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 REMOTE_HOST="nfs_pointless_landing" # This value comes from ~/.ssh/config
-REMOTE_DIR="/home/public"
-LOCAL_DIR="."
+REMOTE_DIR="/home/public/somehash"
+LOCAL_DIR="./build"
+
+echo "Cleaning up remote directory..."
+ssh $REMOTE_HOST "rm -rf $REMOTE_DIR/*"
 
 echo "Uploading files to $REMOTE_HOST:$REMOTE_DIR"
-rsync -avz "$LOCAL_DIR" "$REMOTE_HOST:$REMOTE_DIR"
+rsync -avz --delete "$LOCAL_DIR/" "$REMOTE_HOST:$REMOTE_DIR"
 
 echo "Files uploaded successfully to $REMOTE_HOST:$REMOTE_DIR"
