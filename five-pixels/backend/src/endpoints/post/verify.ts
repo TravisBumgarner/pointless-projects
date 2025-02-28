24ffade54e40e0a6557ffdbc6ed0500106fc196a
Travis Bumgarner <11890057+TravisBumgarner@users.noreply.github.com>
2025-02-27 19:12:20 -0600

    working

--- five-pixels/backend/src/endpoints/post/verify.ts
+++ five-pixels/backend/src/endpoints/post/verify.ts
@@ -2,10 +2,12 @@ import { Request, Response } from "express";
 import { verifyTurnstileToken } from "../../routes/auth";
 
 const verify = async (req: Request, res: Response) => {
+  console.log("verify", req.body);
   const { token } = req.body;
 
   try {
     const isValid = await verifyTurnstileToken(token);
+    console.log("isValid", isValid);
     if (!isValid) {
       return res.status(400).json({ error: "Invalid CAPTCHA" });
     }
