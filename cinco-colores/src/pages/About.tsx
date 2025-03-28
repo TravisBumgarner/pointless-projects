702afb25feaadc68eda716451e3dd33ba7509158
Travis Bumgarner <11890057+TravisBumgarner@users.noreply.github.com>
2025-03-28 10:39:36 -0600

    added rough draft about

--- cinco-colores/src/pages/About.tsx
+++ cinco-colores/src/pages/About.tsx
@@ -1,5 +1,37 @@
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <h1>Sobre</h1>
+      <p>
+        Cuando era chiquito, mi mamá me dijo que podía escoger el color de mi
+        cuarto. Escogí azul oscuro. En la universidad, el dueño de mi casa me
+        dijo lo mismo, y escogí un naranja muy brillante. En un país lleno de
+        paredes blancas, yo era un poquito raro.
+        <p>
+          En 2015, llegué a México por primera vez. La gente de México, sin
+          preguntar a nadie, escogió colores muy brillantes para pintar sus
+          casas. Me enamoré. Cada vez que paso por una casa muy bonita, me
+          siento inspirado a usar esos colores para diseñar un sitio web.
+        </p>
+        <p>Y así nació Cinco Colores.</p>
+        <h1>About</h1>
+        <p>
+          When I was young, my mom told me I could choose the color of my room.
+          I chose dark blue. In university, my landlord told me the same and I
+          chose a bright orange. In a country full of white walls, I felt like
+          an oddity.
+        </p>
+        <p>
+          In 205, I arrived to Mexico for the first time. The people of Mexico,
+          without asking anybody, chose bright colors to paint their houses. I
+          fell in love. Every time I pass by a pretty house, I feel inspired to
+          use those colors to design a website.
+        </p>
+        <p>And thus, Cinco Colors was born.</p>
+      </p>
+      <p>This is the about page.</p>
+    </div>
+  );
 };
 
 export default About;
