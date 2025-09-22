a3f025435952c73fe474ccb2c97786a317d25180
Travis Bumgarner <11890057+TravisBumgarner@users.noreply.github.com>
2025-09-21 19:53:18 -0600

    added nav

--- dice/frontend/src/components/Navigation.tsx
+++ dice/frontend/src/components/Navigation.tsx
@@ -1,8 +1,10 @@
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Link } from "react-router";
-import { FONT_SIZES } from "../styles/styleConsts";
+import { BORDER_RADIUS, FONT_SIZES } from "../styles/styleConsts";
+import { IconButton, Menu, MenuItem, Tooltip } from "@mui/material";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const AVAILABLE_TITLES = [
   "Dapper Dice",
@@ -20,8 +22,52 @@ const AVAILABLE_TITLES = [
   "Downfall Dice",
 ];
 
+const ROUTES = [
+  { key: "home", label: "Home", href: "/", target: "_self" },
+  {
+    key: "github",
+    label: "GitHub",
+    href: "https://github.com/TravisBumgarner/pointless-projects/tree/main/dice",
+    target: "_blank",
+  },
+  {
+    key: "more_projects",
+    label: "More Projects",
+    href: "https://travisbumgarner.com/",
+    target: "_blank",
+  },
+];
+
+const DropdownLinks = ({ onClose }: { onClose: () => void }) => {
+  return (
+    <>
+      {ROUTES.map((route) => (
+        <Link
+          key={route.key}
+          to={route.href}
+          style={{ textDecoration: "none", color: "inherit" }}
+          target={route.target}
+        >
+          <MenuItem onClick={onClose}>{route.label}</MenuItem>
+        </Link>
+      ))}
+    </>
+  );
+};
+
 const Navigation = () => {
   const [title, setTitle] = useState("Dapper Dice");
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = useCallback((event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  }, []);
+
+  const handleClose = useCallback(() => {
+    setAnchorEl(null);
+  }, []);
+
   useEffect(() => {
     const randomIndex = Math.floor(Math.random() * AVAILABLE_TITLES.length);
     const randomTitle = AVAILABLE_TITLES[randomIndex];
@@ -30,7 +76,13 @@ const Navigation = () => {
   }, []);
 
   return (
-    <Box sx={{ marginBottom: "1rem", textAlign: "center" }}>
+    <Box
+      sx={{
+        marginBottom: "1rem",
+        display: "flex",
+        justifyContent: "space-between",
+      }}
+    >
       <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
         <Typography variant="h1">
           {title}
@@ -45,6 +97,26 @@ const Navigation = () => {
           </sup>
         </Typography>
       </Link>
+      <Tooltip title="Menu">
+        <IconButton
+          aria-label="menu"
+          aria-controls={open ? "navigation-menu" : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? "true" : undefined}
+          onClick={handleClick}
+        >
+          <GiHamburgerMenu />
+        </IconButton>
+      </Tooltip>
+      <Menu
+        slotProps={{ paper: { sx: { borderRadius: BORDER_RADIUS.ZERO.PX } } }}
+        id="navigation-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+      >
+        <DropdownLinks onClose={handleClose} />
+      </Menu>
     </Box>
   );
 };
