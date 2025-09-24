96e5bb5f8d0534cbe0c5d556768360a7f8bf9642
Travis Bumgarner <11890057+TravisBumgarner@users.noreply.github.com>
2025-09-23 18:47:31 -0600

    move to one day builds

--- dice/frontend/src/consts.ts
+++
@@ -1,3 +0,0 @@
-export const COLLAPSE_WIDTH = 800
-
-export const DEFAULT_DICE = 20
--- dice/frontend/src/types.ts
+++
@@ -1,6 +0,0 @@
-export interface DiceRollerProps {
-  params: { sides: number }
-  isRolling: boolean
-  setIsRolling: (rolling: boolean) => void
-}
-export type RollerType = 'wheel-of-doom' | 'plinko' | 'balloon' | 'demon-in-the-box'
